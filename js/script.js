f83cde88c310890ba3abe2daa36d35eae8268811
unknown <kasimsarman@gmail.com>
2026-06-23 10:15:59 +0500

    add to slider indicators dots

--- js/script.js
+++ js/script.js
@@ -237,6 +237,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // Slider
 
   const slides = document.querySelectorAll(".offer__slide"),
+    slider = document.querySelector(".offer__slider"),
     prev = document.querySelector(".offer__slider-prev"),
     next = document.querySelector(".offer__slider-next"),
     current = document.querySelector("#current"),
@@ -256,9 +257,63 @@ window.addEventListener("DOMContentLoaded", () => {
     current.textContent = slideIndex < 10 ? `0${slideIndex}` : slideIndex;
 
     slides[slideIndex - 1].style.display = "block";
+
+    dots.forEach((item) => (item.style.opacity = ".5"));
+
+    dots[slideIndex - 1].style.opacity = "1";
   };
 
   const plusSlider = (num) => showSlides((slideIndex += num));
+
+  slider.style.position = "relative";
+
+  const indicators = document.createElement("ol"),
+    dots = [];
+  indicators.classList.add("carousel-indicators");
+  indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `;
+
+  slider.append(indicators);
+
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("li");
+    dot.style.cssText = `
+      box-sizing: content-box;
+      flex: 0 1 auto;
+      width: 30px;
+      height: 6px;
+      margin-right: 3px;
+      margin-left: 3px;
+      cursor: pointer;
+      background-color: #fff;
+      background-clip: padding-box;
+      border-top: 10px solid transparent;
+      border-bottom: 10px solid transparent;
+      opacity: .5;
+      transition: opacity .6s ease;
+    `;
+
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", (e) => {
+      slideIndex = index + 1;
+
+      showSlides();
+    });
+  });
 
   prev.addEventListener("click", () => plusSlider(-1));
 
